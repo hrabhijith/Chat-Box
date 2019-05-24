23f55e56f8f2ef8a843fcff5e83a74617f8d4578
Abhijith <hrabhijith@gmail.com>
2019-05-25 02:09:01 +0530

    local version of the project completed

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 ## Chat Box with emotion detection
 
-* In progress...
+The application lets the users to get registered in to the system and login to the public chat session where an individual can chat with others.
+
+The application also analyzes the phrases submitted each time to the session by the user for emotions using IBM tone analyzer API and appends an appropriate emoticon at the end of each message. 
+
+# Installation and reference to execute from local
+
+* This application is full stack and built using Python Flask with SQLAlchmy ORM and few other modules.
+* Requires Python 3.6.8 [link to download]()
+* First run this command from any IDE in Python interpreter(terminal) to download all the required modules - `pip3 install -r requirements.txt`
+* Then, run this command to setup database objects - `python database_setup.py`
+* Next, run this command and the application starts running and can be viewed in browser at **localhost:1111** - `python main.py`
+* Sigup if new user or login to enter the public chat session and start chatting!!
+* To see two or more users chatting from same browser/machine use **incognito windows**.
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# !/usr/bin/env python3
+# !/usr/bin/python -3.6.8
+
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 from sqlalchemy import create_engine
@@ -16,7 +19,7 @@ import requests
 import json
 from ibm_watson import ToneAnalyzerV3, ApiException
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
 
@@ -30,12 +33,15 @@ def analyzeTone(text):
         url='https://gateway-lon.watsonplatform.net/tone-analyzer/api'
     )
 
-
-    
+    # Tone analyzer API call.
     response = tone_analyzer.tone(text, content_type='text/plain',
-        sentences=True, tones=None, content_language=None, accept_language=None).get_result()
+                                  sentences=True, tones=None,
+                                  content_language=None,
+                                  accept_language=None).get_result()
 
     print(response)
+    return response
+
 
 # Creates sqlite DB connection and return session object
 def dbConnection():
@@ -79,24 +85,26 @@ def signup():
 
         # Checking whether username and email already exists or not
         if uniqueNameFlag == 1 and uniqueEmailFlag == 1:
-            return render_template('signup.html', emailValid=False, nameValid=False)
+            return render_template('signup.html', emailValid=False,
+                                   nameValid=False)
 
         # Checking whether username already exists or not
         elif uniqueNameFlag == 1:
             return render_template('signup.html', nameValid=False)
-        
+
         # Checking whether email already exists or not
         elif uniqueEmailFlag == 1:
             return render_template('signup.html', emailValid=False)
-        
+
         # Creating new user
-        newUser = Users(name=request.form['name'], username=request.form['username'], email=request.form['email'], online=0)
+        newUser = Users(name=request.form['name'],
+                        username=request.form['username'],
+                        email=request.form['email'], online=0)
         newUser.hash_password(request.form['password'])
         session.add(newUser)
         session.commit()
         session.close()
-
-        # on successful registration, redirecting to login 
+        # on successful registration, redirecting to login
         return redirect(url_for('login'))
 
     session.close()
@@ -113,15 +121,19 @@ def login():
     # Creating DB connection
     session = dbConnection()
 
+    # only POST request method functionality
     if request.method == 'POST':
         try:
-            user = session.query(Users).filter_by(email=request.form['email']).one_or_none()
+            user = session.query(Users).filter_by(
+                email=request.form['email']).one_or_none()
         except exc.SQLAlchemyError:
             logging.error('No users data for the give email')
-        
+
+        # Checking whether email exists or not
         if user is None:
-            return render_template('login.html', emailValid = False)
-        
+            return render_template('login.html', emailValid=False)
+
+        # Checking whether password is corrct or not
         if user.verify_password(request.form['password']):
             login_session['id'] = user.id
             login_session['name'] = user.name
@@ -135,7 +147,7 @@ def login():
 
             return redirect(url_for('sessions'))
         else:
-            return render_template('login.html', passwordValid = False)
+            return render_template('login.html', passwordValid=False)
 
     session.close()
     return render_template('login.html')
@@ -147,6 +159,7 @@ def logout():
     # Creating DB connection
     session = dbConnection()
     user = session.query(Users).filter_by(id=login_session['id']).one()
+    # Making the user offline
     user.online = 0
     session.add(user)
     session.commit()
@@ -171,13 +184,13 @@ def sessions():
 
     # Fetching users who are online, messages
     users = session.query(Users).filter_by(online=1).all()
-    
     messagesList = []
     try:
         messages = session.query(Messages).all()
-    except:
+    except exc.SQLAlchemyError:
         messages = None
 
+    # Creating a list of dictionary items to pass to template
     if messages is not None:
         for i in messages:
             messagesDict = {'message': '', 'username': ''}
@@ -185,20 +198,24 @@ def sessions():
             messagesDict['message'] = i.message
             messagesDict['username'] = messageUser.username
             messagesList.append(messagesDict)
+
+        # reversing the order to render properly
+        messagesList = reversed(messagesList)
     else:
         messagesList = None
-    
     session.close()
     return render_template('session.html', users=users, messages=messagesList)
 
 
-
+# On succesful message arrival from client
 def messageReceived(methods=['GET', 'POST']):
     print('message was received!!!')
 
 
+# Socket event route
 @socketio.on('my event')
 def handle_my_custom_event(json, methods=['GET', 'POST']):
+    # appending the username of th user logged in
     json['username'] = login_session['username']
 
     # Creating DB connection
@@ -207,11 +224,38 @@ def handle_my_custom_event(json, methods=['GET', 'POST']):
     # Storing user messages
     user = session.query(Users).filter_by(id=login_session['id']).one_or_none()
 
-    # Calling Watson tone analyzer method
+    # Calling Watson tone analyzer method and analysis of response
     toneResponse = analyzeTone(json['message'])
-    
+    try:
+        t = toneResponse['document_tone']['tones'][0]
+        score = toneResponse['document_tone']['tones'][0]['score']
+        tone = toneResponse['document_tone']['tones'][0]['tone_id']
+        if tone == 'sadness':
+            if score >= 0.8:
+                json['message'] = json['message'] + ' 😭'
+            elif score >= 0.4 and score < 0.8:
+                json['message'] = json['message'] + ' 😢'
+            else:
+                json['message'] = json['message'] + ' 😥'
 
+        elif tone == 'joy':
+            if score >= 0.8:
+                json['message'] = json['message'] + ' 😃'
+            elif score >= 0.4 and score < 0.8:
+                json['message'] = json['message'] + ' 😁'
+            else:
+                json['message'] = json['message'] + ' 😅'
 
+        elif tone == 'anger':
+            if score >= 0.8:
+                json['message'] = json['message'] + ' 💀'
+            elif score >= 0.4 and score < 0.8:
+                json['message'] = json['message'] + ' 😠'
+            else:
+                json['message'] = json['message'] + ' 👿'
+
+    except IndexError:
+        json['message'] = json['message'] + ' 😔'
     newMessage = Messages(message=json['message'], user=user)
     session.add(newMessage)
     session.commit()
